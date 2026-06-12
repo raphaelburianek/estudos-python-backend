668e05c8dc25e2ab6946e48d94fc285062284440
RaphaelBurianek <raphael.burianek@gmail.com>
2026-06-12 14:48:20 -0300

    Primeiro desafio do curso

---
+++ desafio1.py
@@ -0,0 +1,3 @@
+#Desafio é criar um script que pergunte o nome do usuario e exiba uma mensgaem de boas vindas 
+nome = input('Qual o seu nome? ')
+print('Olá,', nome, ' seja bem-vindo ao meu primeiro desafio.')
