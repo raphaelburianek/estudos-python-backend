b9d57783e634086bfd2a39532672d04437f8b12c
RaphaelBurianek <raphael.burianek@gmail.com>
2026-06-12 14:38:35 -0300

    Meu primeiro contato com Python - Print, Input e Variáveis

---
+++ teste.py
@@ -0,0 +1,12 @@
+print('Olá Mundo!') #print exibe na tela
+nome = 'Raphael' 
+idade = 32
+peso = 81.3
+print(nome, idade, peso) 
+
+nome1 = input ('Qual é o seu nome? ') #input le oque o usuario digitar
+idade1 = input ('Qual a sua idade? ') #tudo que for lido pelo codigo so com input vai sertratado como string
+peso1 = input ('Qual o seu peso? ')
+
+print('Olá,', nome1, ' você tem ', idade1, ' anos, e pesa ', peso1)
+
