9863c798d25edbcce705f5df1f7de6184384751f
RaphaelBurianek <raphael.burianek@gmail.com>
2026-06-12 14:57:46 -0300

    desafio 2 do curso

---
+++ desafio2.py
@@ -0,0 +1,6 @@
+#desafio 2 perguntar o dia o mes e ano do nascimento do usuario e exibir uma mensagem formatada no terminal.
+dia = input('Qual o dia do seu nascimento? ')
+mes = input('Qual o mes do seu nascimento? ')
+ano = input('Qual o ano do seu nascimento? ')
+
+print(f'Você nasceu no dia, {dia} de {mes} de {ano}')
