e674125701fd1b525ea5f5e41d1fda060413256f
RaphaelBurianek <raphael.burianek@gmail.com>
2026-06-12 15:07:01 -0300

    desafio 3 tentar fazer o scripy somar 2 numeros iseridos pelo usuario

---
+++ desafio3.py
@@ -0,0 +1,4 @@
+#tentar pedir 2 numeros ao usuario e somar esses numeros.
+num1 = int( input('digite um numero? '))
+num2 = int( input('digite outro numero'))
+print('a soma dos dois numeros é: ', num1+num2)
